cadfb91d55b4c19875446c083374679d2e583595
adityamore7000 <adi.more7000@gmail.com>
2024-03-09 18:11:47 +0530

    routing, nested and groups done

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Title from '$lib/components/Title.svelte';
+    import '../../styles/global.css';
+    const year = new Date().getFullYear();
+    </script>
+
+
+<header>
+    <Title />
+</header>
+  
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p> Copyright &copy; {year} </p>
+</footer>
+
+<style>
+    header {
+      display: flex;
+      justify-content: center;
+    }
+    main {
+      max-width: 960px;
+      margin: 20px auto;
+    }
+    footer {
+      text-align: center;
+    }
+  </style>
